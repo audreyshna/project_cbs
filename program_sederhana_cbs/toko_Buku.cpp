a7ff27397dd2a725e62c6b946cce1996827fc44f
Pi0piuw <shofy.aliya@gmail.com>
2023-10-09 16:11:29 +0700

    push_shofy

--- program_sederhana_cbs/toko_Buku.cpp
+++ program_sederhana_cbs/toko_Buku.cpp
@@ -28,17 +28,17 @@ void daftar_buku_fantasi(){
     while (option)
     {
         system("cls");
-        harga_1 = 18000;
-        harga_2 = 18000;
-        harga_3 = 18000;
-        harga_4 = 18000;
-        harga_5 = 18000;
+        harga_1 = 140000;
+        harga_2 = 80000;
+        harga_3 = 130000;
+        harga_4 = 135000;
+        harga_5 = 80000;
 
-        cout << "1.\t" << harga_1 << endl;
-        cout << "2.\t" << harga_2 << endl;
-        cout << "3.\t" << harga_3 << endl;
-        cout << "4.\t" << harga_4 << endl;
-        cout << "5.\t" << harga_5 << endl;
+        cout << "Harry Potter\t" << harga_1 << endl;
+        cout << "Bumi\t" << harga_2 << endl;
+        cout << "Hunger Games\t" << harga_3 << endl;
+        cout << "Six Of Crows\t" << harga_4 << endl;
+        cout << "Ceros dan Batozar\t" << harga_5 << endl;
         cout << "=========================" << endl;
         cin >> input_user;
         switch (input_user)
@@ -122,17 +122,17 @@ void daftar_buku_misteri(){
     while (option)
     {
         system("cls");
-        harga_1 = 18000;
-        harga_2 = 18000;
-        harga_3 = 18000;
-        harga_4 = 18000;
-        harga_5 = 18000;
+        harga_1 = 109000;
+        harga_2 = 80000;
+        harga_3 = 75000;
+        harga_4 = 109000;
+        harga_5 = 80000;
 
-        cout << "1.\t" << harga_1 << endl;
-        cout << "2.\t" << harga_2 << endl;
-        cout << "3.\t" << harga_3 << endl;
-        cout << "4.\t" << harga_4 << endl;
-        cout << "5.\t" << harga_5 << endl;
+        cout << "The Guardians\t" << harga_1 << endl;
+        cout << "Sherlock Holmes\t" << harga_2 << endl;
+        cout << "Detective Conan\t" << harga_3 << endl;
+        cout << "One of Us Is Lying\t" << harga_4 << endl;
+        cout << "Pembunuhan Zodiak Tokyo\t" << harga_5 << endl;
         cout << "=========================" << endl;
         cin >> input_user;
         switch (input_user)
