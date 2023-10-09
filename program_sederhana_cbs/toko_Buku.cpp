b99a2aa1aaa3d2bbc66ab88de1a026344f6aa61d
Bagas Diatama <diatama.w8@outlook.co.id>
2023-10-09 15:24:02 +0700

    feature_update_2

--- program_sederhana_cbs/toko_Buku.cpp
+++ program_sederhana_cbs/toko_Buku.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -12,7 +13,15 @@ int jumlah_harga(int harga){
     
 }
 
-int kupon_diskon();
+int kupon_diskon(string kode_kupon){
+    
+    int jumlah_diskon;
+
+    if (kode_kupon = "FANTASTIC5"){
+        jumlah_diskon = (jumlah_harga(0) * 20) / 100;
+        return jumlah_diskon;
+    }
+}
 
 void halaman_depan(){
 
@@ -23,75 +32,192 @@ void halaman_depan(){
 }
 
 void daftar_buku_fantasi(){
-    system("cls");
-    int harga_1, harga_2, harga_3, harga_4, harga_5;
-    harga_1 = 18000;
-    harga_2 = 18000;
-    harga_3 = 18000;
-    harga_4 = 18000;
-    harga_5 = 18000;
+   system("cls");
+    int harga_1, harga_2, harga_3, harga_4, harga_5, input_user;
+    bool option = 1;
+    while (option)
+    {
+        system("cls");
+        harga_1 = 18000;
+        harga_2 = 18000;
+        harga_3 = 18000;
+        harga_4 = 18000;
+        harga_5 = 18000;
 
-    cout << "1.\t" << harga_1 << endl;
-    cout << "2.\t" << harga_2 << endl;
-    cout << "3.\t" << harga_3 << endl;
-    cout << "4.\t" << harga_4 << endl;
-    cout << "5.\t" << harga_5 << endl;
-    cout << "=========================" << endl;
-            
+        cout << "1.\t" << harga_1 << endl;
+        cout << "2.\t" << harga_2 << endl;
+        cout << "3.\t" << harga_3 << endl;
+        cout << "4.\t" << harga_4 << endl;
+        cout << "5.\t" << harga_5 << endl;
+        cout << "=========================" << endl;
+        cin >> input_user;
+        switch (input_user)
+        {
+        case 1:
+            jumlah_harga(harga_1);
+            break;
+        case 2:
+            jumlah_harga(harga_2);
+            break;
+        case 3:
+            jumlah_harga(harga_3);
+            break;
+        case 4:
+            jumlah_harga(harga_4);
+            break;
+        case 5:
+            jumlah_harga(harga_5);
+            break;
+        case 6:
+            option = 0;
+            break;
+        default:
+            option = 0;
+            break;
+        }
+    }
 }
 
 void daftar_buku_politik(){
-    system("cls");
-    int harga_1, harga_2, harga_3, harga_4, harga_5;
-    harga_1 = 18000;
-    harga_2 = 18000;
-    harga_3 = 18000;
-    harga_4 = 18000;
-    harga_5 = 18000;
+   system("cls");
+    int harga_1, harga_2, harga_3, harga_4, harga_5, input_user;
+    bool option = 1;
+    while (option)
+    {
+        system("cls");
+        harga_1 = 18000;
+        harga_2 = 18000;
+        harga_3 = 18000;
+        harga_4 = 18000;
+        harga_5 = 18000;
 
-    cout << "1.\t" << harga_1 << endl;
-    cout << "2.\t" << harga_2 << endl;
-    cout << "3.\t" << harga_3 << endl;
-    cout << "4.\t" << harga_4 << endl;
-    cout << "5.\t" << harga_5 << endl;
-    cout << "=========================" << endl;
-            
+        cout << "1.\t" << harga_1 << endl;
+        cout << "2.\t" << harga_2 << endl;
+        cout << "3.\t" << harga_3 << endl;
+        cout << "4.\t" << harga_4 << endl;
+        cout << "5.\t" << harga_5 << endl;
+        cout << "=========================" << endl;
+        cin >> input_user;
+        switch (input_user)
+        {
+        case 1:
+            jumlah_harga(harga_1);
+            break;
+        case 2:
+            jumlah_harga(harga_2);
+            break;
+        case 3:
+            jumlah_harga(harga_3);
+            break;
+        case 4:
+            jumlah_harga(harga_4);
+            break;
+        case 5:
+            jumlah_harga(harga_5);
+            break;
+        case 6:
+            option = 0;
+            break;
+        default:
+            option = 0;
+            break;
+        }
+    }
 }
 
 void daftar_buku_misteri(){
-    system("cls");
-    int harga_1, harga_2, harga_3, harga_4, harga_5;
-    harga_1 = 18000;
-    harga_2 = 18000;
-    harga_3 = 18000;
-    harga_4 = 18000;
-    harga_5 = 18000;
+   system("cls");
+    int harga_1, harga_2, harga_3, harga_4, harga_5, input_user;
+    bool option = 1;
+    while (option)
+    {
+        system("cls");
+        harga_1 = 18000;
+        harga_2 = 18000;
+        harga_3 = 18000;
+        harga_4 = 18000;
+        harga_5 = 18000;
 
-    cout << "1.\t" << harga_1 << endl;
-    cout << "2.\t" << harga_2 << endl;
-    cout << "3.\t" << harga_3 << endl;
-    cout << "4.\t" << harga_4 << endl;
-    cout << "5.\t" << harga_5 << endl;
-    cout << "=========================" << endl;
+        cout << "1.\t" << harga_1 << endl;
+        cout << "2.\t" << harga_2 << endl;
+        cout << "3.\t" << harga_3 << endl;
+        cout << "4.\t" << harga_4 << endl;
+        cout << "5.\t" << harga_5 << endl;
+        cout << "=========================" << endl;
+        cin >> input_user;
+        switch (input_user)
+        {
+        case 1:
+            jumlah_harga(harga_1);
+            break;
+        case 2:
+            jumlah_harga(harga_2);
+            break;
+        case 3:
+            jumlah_harga(harga_3);
+            break;
+        case 4:
+            jumlah_harga(harga_4);
+            break;
+        case 5:
+            jumlah_harga(harga_5);
+            break;
+        case 6:
+            option = 0;
+            break;
+        default:
+            option = 0;
+            break;
+        }
+    }
             
 }
 
 void daftar_buku_horor(){
     system("cls");
-    int harga_1, harga_2, harga_3, harga_4, harga_5;
-    harga_1 = 18000;
-    harga_2 = 18000;
-    harga_3 = 18000;
-    harga_4 = 18000;
-    harga_5 = 18000;
+    int harga_1, harga_2, harga_3, harga_4, harga_5, input_user;
+    bool option = 1;
+    while (option)
+    {
+        system("cls");
+        harga_1 = 18000;
+        harga_2 = 18000;
+        harga_3 = 18000;
+        harga_4 = 18000;
+        harga_5 = 18000;
 
-    cout << "1.\t" << harga_1 << endl;
-    cout << "2.\t" << harga_2 << endl;
-    cout << "3.\t" << harga_3 << endl;
-    cout << "4.\t" << harga_4 << endl;
-    cout << "5.\t" << harga_5 << endl;
-    cout << "=========================" << endl;
-            
+        cout << "1.\t" << harga_1 << endl;
+        cout << "2.\t" << harga_2 << endl;
+        cout << "3.\t" << harga_3 << endl;
+        cout << "4.\t" << harga_4 << endl;
+        cout << "5.\t" << harga_5 << endl;
+        cout << "=========================" << endl;
+        cin >> input_user;
+        switch (input_user)
+        {
+        case 1:
+            jumlah_harga(harga_1);
+            break;
+        case 2:
+            jumlah_harga(harga_2);
+            break;
+        case 3:
+            jumlah_harga(harga_3);
+            break;
+        case 4:
+            jumlah_harga(harga_4);
+            break;
+        case 5:
+            jumlah_harga(harga_5);
+            break;
+        case 6:
+            option = 0;
+            break;
+        default:
+            option = 0;
+            break;
+        }
+    }
 }
 
 void daftar_buku_romantis(){
@@ -139,9 +265,6 @@ void daftar_buku_romantis(){
             break;
         }
     }
-    
-    
-
 }
 
 int main(){
